6514aa9a8f131e81a4556791a0c3929f535270ce
ildrasa <ildrasa@kalash-PC.home>
2018-02-19 00:13:51 +0100

    [add] add shell script containing basic tests for brunehilde

---
+++ integration_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FIRST_CURL_RESULT = $(curl http://127.0.0.1:5002/)
+
+SECOND_CURL_RESULT = $(curl http://127.0.0.1:5002/)
+
+if ["$FIRST_RESULT" = "Hello World! I have been seen 1 times."]
+	then 
+		if ["$SECOND_CURL_RESULT" = "Hello World! I have been seen 2 times."]
+			then
+				exit 0
+			else
+				exit -1
+		fi
+	else
+		exit -1
+fi
