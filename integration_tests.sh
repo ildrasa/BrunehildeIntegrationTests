37cc16f4d810378788299475061ec451a2e71261
ildrasa <ildrasa@kalash-PC.home>
2018-02-19 01:44:29 +0100

    [fix] fix script

--- integration_tests.sh
+++ integration_tests.sh
@@ -5,7 +5,7 @@ EXPECTED_FIRST_CURL="Hello World! I have been seen 1 times."
 SECOND_CURL_RESULT=$(curl -sb -H http://127.0.0.1:5002/)
 EXPECTED_SECOND_CURL="Hello World! I have been seen 2 times."
 
-if [ "$FIRST_CURL_RESULT" == "$EXPECTED_FIRST_CURL" ]
+if [ "$FIRST_CURL_RESULT" = "$EXPECTED_FIRST_CURL" ]
 	then 
 		if [ "$SECOND_CURL_RESULT" = "$EXPECTED_SECOND_CURL" ]
 			then
