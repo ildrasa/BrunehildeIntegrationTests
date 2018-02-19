b6da256e55c6f91d0aad2972080f3910b398ac5b
ildrasa <ildrasa@kalash-PC.home>
2018-02-19 01:10:28 +0100

    [fix] fix script

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-$FIRST_CURL_RESULT = $(curl http://127.0.0.1:5002/)
+FIRST_CURL_RESULT=$(curl -sb -H http://127.0.0.1:5002/)
+EXPECTED_FIRST_CURL="Hello World! I have been seen 1 times."
+SECOND_CURL_RESULT=$(curl -sb -H http://127.0.0.1:5002/)
+EXPECTED_SECOND_CURL="Hello World! I have been seen 2 times."
 
-$SECOND_CURL_RESULT = $(curl http://127.0.0.1:5002/)
-
-if ["$FIRST_RESULT" = "Hello World! I have been seen 1 times."]
+if [ "$FIRST_CURL_RESULT" = "$EXPECTED_FIRST_CURL" ]
 	then 
-		if ["$SECOND_CURL_RESULT" = "Hello World! I have been seen 2 times."]
+		if [ "$SECOND_CURL_RESULT" = "$EXPECTED_SECOND_CURL" ]
 			then
 				exit 0
 			else
